b920197634f746fe854ecddc1bbc60c8036935dd
alialkaheli <ali.alkaheli96@gmail.com>
2019-01-10 16:11:27 -0800

    Paddle class to design shape of ball

---
+++ src2/paddle.js
@@ -0,0 +1,22 @@
+class Paddle{
+    constructor(width, height, ctx){
+        this.paddleW = 75;
+        this.paddleH = 10
+        this.paddlex = (width - this.paddleW) / 2;
+        this.width = width;
+        this.height = height;
+
+        this.ctx = ctx;
+    }
+
+    drawPaddle() {
+        this.ctx.beginPath();
+        this.ctx.rect(this.paddlex, this.height - this.paddleH, this.paddleW, this.paddleH);
+        this.ctx.fillStyle = "black";
+        this.ctx.fill();
+        this.ctx.closePath();
+
+    }
+}
+
+export default Paddle;
