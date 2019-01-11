7688d5b8bc8d53d85c4e2a085bc1a60ce8085476
alialkaheli <ali.alkaheli96@gmail.com>
2019-01-10 16:06:41 -0800

    webpack and bundle files set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src2/mainGame.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        filename: 'bundle.js',
+        path: path.join(__dirname, "./")
+    },
+    devtool: 'source-map'
 };
