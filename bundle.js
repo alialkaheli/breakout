d8d9fc129e73144458619f42617fb5dc0d68ec71
ali alkaheli <ali.alkaheli96@gmail.com>
2019-02-18 15:40:57 -0800

    bundle file

--- bundle.js
+++ bundle.js
@@ -3639,16 +3639,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // // console.log("canvas2");
     // let ctx = canvas.getContext('2d');
     var sound = new howler__WEBPACK_IMPORTED_MODULE_1__["Howl"]({
-        src: ['/src2/sound.mp3'],
+        src: ['./src2/sound.mp3'],
         buffer: true,
         loop: true
     });
     var brick = new howler__WEBPACK_IMPORTED_MODULE_1__["Howl"]({
-      src: ["/src2/breaking.mp3"],
+      src: ["./src2/breaking.mp3"],
       
     });
     var bounce = new howler__WEBPACK_IMPORTED_MODULE_1__["Howl"]({
-        src: ["/src2/bounce.mp3"],
+        src: ["./src2/bounce.mp3"],
 
     });
 
